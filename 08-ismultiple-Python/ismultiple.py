f316007a286dd2d9cd4dbd111fbc3bb0c91aeab9
srikanth <you@example.com>
2021-07-12 23:44:26 +0530

    done

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -6,4 +6,14 @@
 
 
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if (m==n):
+		return True
+	elif (m==0):
+		return True
+	elif (n==0):
+		return False
+	elif (m%n==0):
+		return True
+	else:
+		return False					
+	
