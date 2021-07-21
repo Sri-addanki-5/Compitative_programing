1fed19ba7b4137e1528975586d167f80b6ed15d0
srikanth <you@example.com>
2021-07-21 22:24:44 +0530

    done

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -5,6 +5,53 @@
 # For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
 
 
+import math
+
+def isPowerful(n):
+    while (n % 2 == 0):
+        a = 0
+        while (n % 2 == 0):
+            n = n // 2
+            a = a + 1
+        if ( a == 1):
+            return False
+    for power in range(3, int(math.sqrt(n))+1, 2):
+        a = 0
+        while (n % power == 0):
+            n = n // power
+            a = a + 1
+        if (a == 1):
+            return False
+    return (n == 1)
+
 def nthpowerfulnumber(n):
-	# Your code goes here
-	pass
+    # Your code goes here
+    x = 0
+    y = 0
+    while (x <= abs(n)):
+        y += 1
+        if(isPowerful(y)):
+           x += 1
+    return y
+
+
+# Sum = 0
+# Temp = n
+# while(Temp > 0):
+#     Reminder = Temp % 10
+#     Factorial = math.factorial(Reminder)
+
+#     print("Factorial of %d = %d" %(Reminder, Factorial))
+#     Sum = Sum + Factorial
+#     Temp = Temp // 10
+# return n, sum
+# # print("\n Sum of Factorials of a Given Number %d = %d" %(n, Sum))
+    
+# # if (Sum == n):
+# #     print(" %d is a Strong Number" %n)
+# # else:
+# #     print(" %d is not a Strong Number" %Number)
+
+# import math
+# def nthpowerfulnumber(n):
+#     # Your code goes here
