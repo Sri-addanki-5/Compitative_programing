38a1cbc6138c1d34fb6d30c7f525274cdc0dd5ec
srikanth <you@example.com>
2021-08-05 10:13:22 +0530

    done

---
+++ limitedPowerSet/set.py
@@ -0,0 +1,23 @@
+# limitedPowerSet(n, k)
+# Write a function limitedPowerSets(n, k) that takes possibly 
+# a non-negative integer n and k and returns the list that 
+# contains k number of subsets from the power set as sets. 
+# (The values in the set will range from 1 to n).
+# Example:
+# 	limitedPowerSet(5, 7) => 
+# [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
+
+def limitedPowerSet(n, k):
+    input_list = []
+    res = [{}]
+    for i in range(1,n+1):
+        input_list.append(i)
+        res.append({i})
+
+    for j in range(1, 1<< n):
+        result = []
+        result.append({input_list[m] for m in range(n) if (j & (1 << m))}) 
+        for z in result:
+            if z not in res:
+                res.append(z)
+    return res[:k]               
